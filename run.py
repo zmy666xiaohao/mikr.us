cc59ea93a7a17cc3113d64b6bddf0aa5b64924d7
zmy666xiaohao <zhangrongquanzhangmuyang@gmail.com>
2025-04-15 10:47:58 +0800

    Update run.py

--- run.py
+++ run.py
@@ -14,7 +14,7 @@ def ssh_multiple_connections(hosts_info, command):
         try:
             ssh = paramiko.SSHClient()
             ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            ssh.connect(hostname=hostname, port=10345, username=username, password=password)
+            ssh.connect(hostname=hostname, port=12139, username=username, password=password)
             stdin, stdout, stderr = ssh.exec_command(command)
             user = stdout.read().decode().strip()
             users.append(user)
